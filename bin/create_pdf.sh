1b6c0f3aa64e79a339c05fe8963285d29bbb7f88
Filip Oščádal <f@mxd.cz>
2023-11-15 21:01:14 +0100

    minor update

--- bin/create_pdf.sh
+++ bin/create_pdf.sh
@@ -7,5 +7,6 @@ command -v docker >/dev/null 2>&1 || fail "Docker is NOT installed!"
 
 find . -maxdepth 1 -iname "*.md" -exec echo "converting {} to ADOC ..." \; -exec docker run --rm -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o "{}.adoc" \;
 find . -maxdepth 1 -iname "*.adoc" -exec echo "converting {} to PDF ..." \; -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -a allow-uri-read -d book "{}" \;
+find . -maxdepth 1 -iname "*.adoc" -delete
 
 exit 0
