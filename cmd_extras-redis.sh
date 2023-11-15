1ff463baa708181c2b84aea0e3559a59614635ff
Filip Oščádal <f@mxd.cz>
2023-11-15 21:07:43 +0100

    minor update

--- cmd_extras-redis.sh
+++ cmd_extras-redis.sh
@@ -2,6 +2,8 @@
 # Fred Brooker <git@gscloud.cz>
 
 apt-get -yqq update
-apt-get install -y redis-server && pecl install redis && docker-php-ext-enable redis
+apt-get -o DPkg::Lock::Timeout=30 install -y less redis-server
+pecl install redis
+docker-php-ext-enable redis
 
 exit 0
--- cmd_extras-soap.sh
+++ cmd_extras-soap.sh
@@ -3,7 +3,6 @@
 
 apt-get -yqq update
 apt-get -o DPkg::Lock::Timeout=30 install -y less libxml2-dev
-
 docker-php-ext-install soap
 
 php -i | grep -i soap
